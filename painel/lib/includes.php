a72303bda1ed293d900af532d72cebbf3ffb1959
tamerelmenoufi <tamer@mohatron.com.br>
2024-05-23 11:28:57 -0400

    Atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -3,7 +3,7 @@
 
     // include("connect_local.php");
 
-    include("/sisBuscainc/connect.php");
+    include("/sisBuscasinc/connect.php");
     $con = AppConnect('app');
 
     // include("/appinc/connect.php");
