ce00609c53f707b5759a8f2ddec889e96dc5690f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-26 14:50:22 +0300

    Atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -11,6 +11,23 @@
 
     $md5 = md5(date("YmdHis"));
 
+
+    if($_POST['historico']){
+        echo $_SERVER["PHP_SELF"];
+        $pagina = str_replace("/bkManaus/app/", false, $_SERVER["PHP_SELF"]);
+        $destino = $_POST['historico'];
+        $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
+        if($_SESSION['historico'][$i]['local'] != $pagina){
+            $j = (($_SESSION['historico'][$i]['local'])?($i+1):0);
+            $_SESSION['historico'][$j]['local'] = $pagina;
+            $_SESSION['historico'][$j]['destino'] = $_POST['historico'];
+            unset($_POST['historico']);
+            $_SESSION['historico'][$j]['dados'] = json_encode($_POST);
+        }else{
+            unset($_POST['historico']);
+        }
+    }
+
     include("dicionary_".(($_SESSION['lng'])?$_SESSION['lng']:'en').".php");
 
     $localPainel = $_SERVER["REQUEST_SCHEME"]."://cheappanel.mohatron.com/";
