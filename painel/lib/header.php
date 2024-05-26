608181aacfa31755beb024af73d60b98a31fc772
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-26 14:11:24 +0300

    Atualizações

--- painel/lib/header.php
+++ painel/lib/header.php
@@ -6,23 +6,23 @@
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&callback=initMap&v=weekly&language=pt&region=BR"
 ></script>
 
-<link href="<?=$urlPainel?>lib/vendor/bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link href="<?=$urlPainel?>lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.css" rel="stylesheet" >
+<link href="<?=$localPainel?>lib/vendor/bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link href="<?=$localPainel?>lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.css" rel="stylesheet" >
 
 
-<script src="<?=$urlPainel?>lib/vendor/jquery-3.6.0/jquery-3.6.0.min.js" ></script>
-<script src="<?=$urlPainel?>lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.js" ></script>
+<script src="<?=$localPainel?>lib/vendor/jquery-3.6.0/jquery-3.6.0.min.js" ></script>
+<script src="<?=$localPainel?>lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.js" ></script>
 
-<link href="<?=$urlPainel?>lib/vendor/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+<link href="<?=$localPainel?>lib/vendor/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
 
-<script src="<?=$urlPainel?>lib/vendor/jQuery-Mask/jquery.mask.min.js" ></script>
+<script src="<?=$localPainel?>lib/vendor/jQuery-Mask/jquery.mask.min.js" ></script>
 
-<script src="<?=$urlPainel?>lib/vendor/chart/chart.min.js" ></script>
+<script src="<?=$localPainel?>lib/vendor/chart/chart.min.js" ></script>
 
-<link href="<?=$urlPainel?>lib/css/app.css" rel="stylesheet">
-<script src="<?=$urlPainel?>lib/js/app.js?<?=date("YmdHis")?>" ></script>
+<link href="<?=$localPainel?>lib/css/app.css" rel="stylesheet">
+<script src="<?=$localPainel?>lib/js/app.js?<?=date("YmdHis")?>" ></script>
 
-<link href="<?=$urlPainel?>lib/vendor/slick/slick.css" rel="stylesheet">
-<script src="<?=$urlPainel?>lib/vendor/slick/slick.js" ></script>
+<link href="<?=$localPainel?>lib/vendor/slick/slick.css" rel="stylesheet">
+<script src="<?=$localPainel?>lib/vendor/slick/slick.js" ></script>
 
 
