fb1f5712b339910a37408322563a2923b8bd127f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-26 23:06:38 +0300

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -51,12 +51,32 @@
 ?>
     <div class="card m-3 nb-0">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title"><?=$d->name?></h5>
     </div>
-    <img src="http://cheappanel.mohatron.com/src/volume/products/1/a55f6be79878b5e34ef5aaf3a9b80fa4.png" class="card-img-bottom" alt="...">
+
+    <div class="row mb-3">
+        <?php
+            $path = "{$_SERVER['DOCUMENT_ROOT']}/painel/src/volume/products/{$d->id}/";
+            if(is_dir($path)){
+            $diretorio = dir($path);
+            while($arquivo = $diretorio -> read()){
+                if(is_file($path.$arquivo)){
+        ?>
+            <div class="col-4">
+                <img src="src/volume/products/<?="{$_POST['id']}/{$arquivo}"?>" class="img-fluid m-3">
+            </div>
+        <?php
+                }
+            }
+            $diretorio -> close();
+            }
+        ?>
+    </div>
+
+    <!-- <img src="http://cheappanel.mohatron.com/src/volume/products/1/a55f6be79878b5e34ef5aaf3a9b80fa4.png" class="card-img-bottom" alt="..."> -->
     <div class="card-body">
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        <p class="card-text"><?=$d->description?></p>
+        <p class="card-text"><small class="text-body-secondary"><?=dataBr($d->end_date)?></small></p>
     </div>
     </div>    
 <?php
