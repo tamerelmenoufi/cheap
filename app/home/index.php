f44a1264aaebdda5b3c51a5edfffd7a0784cef93
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-05-04 13:25:34 +0300

    first commit

--- app/home/index.php
+++ app/home/index.php
@@ -94,7 +94,10 @@
 </div>
 
 <div class="barraBusca">
-
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </div>
 </div>
 
 <div class="home_corpo">
